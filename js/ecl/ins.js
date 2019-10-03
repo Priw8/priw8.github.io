71f7eea4612b2d08944d05304a9013f4b344ac46
Priw8 <priwe8@gmail.com>
2019-10-03 15:57:21 +0200

    finish TD+ ins 300 series

--- js/ecl/ins.js
+++ js/ecl/ins.js
@@ -128,6 +128,69 @@ const INS =
 		description: "Sets the execution pointer of ANM script on slot %1 to where ANM `ins_5(a)` with `a` == `switch` is (the time is also changed to match time of `ins_5`). Doesn't work if the value is 0 apparently?"
 	},
 	{
+		number: 318,
+		game: 13,
+		args: "",
+		argnames: [],
+		description: "Resets some ANM-related parameters of the caller (TODO: what exactly?)"
+	},
+	{
+		number: 319,
+		game: 13,
+		args: "Sf",
+		argnames: ["slot", "r"],
+		description: "Set rotation of an ANM script on slot %1 to %2."
+	},
+	{
+		number: 320,
+		game: 13,
+		args: "Sff",
+		argnames: ["slot", "x", "y"],
+		description: "Set position of an ANM script on slot %1 to (%2,%3). This does not detach the script from the caller, so the actual position of the script will be position of the caller + position set here."
+	},
+	{
+		number: 321,
+		game: 13,
+		args: "mffSSS",
+		argnames: ["sub", "x", "y", "hp", "score", "item"],
+		description: "[ins=300,13], but used by 'MapleEnemy' (TODO: what's the difference from [ins=300,13]?)"
+	},
+	{
+		number: 322,
+		game: 13,
+		args: "SS",
+		argnames: ["a", "b"],
+		description: "[c=red]Unknown.[/c]"
+	},
+	{
+		number: 323,
+		game: 13,
+		args: "SS",
+		argnames: ["a", "b"],
+		description: "[c=red]Unknown.[/c]"
+	},
+	{
+		number: 324,
+		game: 13,
+		args: "",
+		argnames: [],
+		description: "[c=red]Unknown.[/c]"
+	},
+	{
+		number: 325,
+		game: 13,
+		args: "SSSS",
+		argnames: ["slot", "R", "G", "B"],
+		description: "Modify color of ANM script on slot %1, [c=red]%2[/c], [c=lightgreen]%3[/c], [c=lightblue]%4[/c] correspond to individual colors and have to be from 0 to 255."
+	},
+	{
+		number: 326,
+		game: 13,
+		args: "SSSSSS",
+		argnames: ["slot", "time", "mode", "R", "G", "B"],
+		description: "Same as [ins=325,13], but the change is applied in %2 frames using mode %3."
+	},
+	{
 		number: 327,
 		game: 13,
 		args: "SS",
@@ -139,7 +202,99 @@ const INS =
 		game: 13,
 		args: "SSSS",
 		argnames: ["slot", "time", "mode", "alpha"],
-		description: "Change alpha of ANM script on slot %1 to %4 (must be a value between 0-255) in %2 frames using mode %3."
+		description: "Same as [ins=327,13], but the change is applied in %2 frames using mode %3."
+	},
+	{
+		number: 329,
+		game: 13,
+		args: "Sff",
+		argnames: ["slot", "w", "h"],
+		description: "Sets horizontal and vertical scaling of ANM script on slot %1 to %2 and %3 respectively. Overwrites scaling properties set by the ANM script."
+	},
+	{
+		number: 330,
+		game: 13,
+		args: "SSSff",
+		argnames: ["slot", "time", "mode", "w", "h"],
+		description: "Same as [ins=329,13], but the change is applied in %2 frames using mode %3."
+	},
+	{
+		number: 331,
+		game: 13,
+		args: "SS",
+		argnames: ["a", "b"],
+		description: "[c=red]Unknown.[/c] Might be doing something to ANM script on slot %1."
+	},
+	{
+		number: 332,
+		game: 13,
+		args: "SSSS",
+		argnames: ["a", "b", "c", "d"],
+		description: "[c=red]Unknown.[/c] Might be doing something to ANM script on slot %1."
+	},
+	{
+		number: 333,
+		game: 13,
+		args: "SSSff",
+		argnames: ["a", "b", "c", "r", "s"],
+		description: "[c=red]Unknown.[/c] Might be doing something to ANM script on slot %1."
+	},
+	{
+		number: 334,
+		game: 13,
+		args: "S",
+		argnames: ["a"],
+		description: "[c=red]Hard to tell?[/c] THBWiki Google translate gives this: 'The animation effect of the playing unit (enemy), the lightning effect near the evil spirit ball of the god temple', which might be refering to lightning around Nue in [game=13]TD[/game] extra stage."
+	},
+	{
+		number: 335,
+		game: 13,
+		args: "Sff",
+		argnames: ["slot", "w", "h"],
+		description: "Same as [ins=329,13], except it doesn't overwrite the scaling set by the ANM script - instead, it multiplies it."
+	},
+
+	{
+		number: 336,
+		game: 14,
+		args: "SS",
+		argnames: ["a", "b"],
+		description: "[c=red]Unknown.[/c]"
+	},
+	{
+		number: 337,
+		game: 16,
+		args: "SS",
+		argnames: ["slot", "b"],
+		description: "Set blend mode of ANM script on slot %1 to %2 (TODO: make a list of blend modes)."
+	},
+	{
+		number: 337,
+		game: 14,
+		args: "SSfff",
+		argnames: ["a", "b", "r", "s", "m"],
+		description: "[c=red]Unknown.[/c]"
+	},
+	{
+		number: 338,
+		game: 16,
+		args: "SSfff",
+		argnames: ["anmIndex", "script", "x", "y", "z"],
+		description: "Same as [ins=314,16], but the spawned script is offset by (%3,%4). For 3D objects, %5 is used too (TODO: verify that)."
+	},
+	{
+		number: 339,
+		game: 16,
+		args: "SSS",
+		argnames: ["a", "b", "c"],
+		description: "[c=red]Unknown.[/c] Might be doing something to ANM script on slot %1."
+	},
+	{
+		number: 340,
+		game: 16,
+		args: "S",
+		argnames: ["id"],
+		description: "[c=red]Unknown.[/c] Used in [game=16]HSiFS[/game] stage 6, %1 is an ID of another enemy. Might be used to delete that enemy."
 	},
 	/* 400 SERIES */
 	{
