6a94bbd7bec67de23926dd47a0e3bbf1be318ebb
Priw8 <44707079+Priw8@users.noreply.github.com>
2019-12-14 12:09:45 +0100

    implement new eclmap format

--- js/ecl/eclmap.js
+++ js/ecl/eclmap.js
@@ -79,9 +79,24 @@ async function autoEclmap(game) {
 
 class Eclmap {
     constructor(txt) {
+        this.is_new = false;
+
+        // old eclmaps
         this.opcodes = {};
         this.timeline_opcodes = {};
         this.globals = {};
+
+        // new ecmlaps
+        this.ins = [];
+        this.timeline_ins = [];
+        this.var = [];
+        this.ins_sig = [];
+        this.timeline_ins_sig = [];
+        this.var_types = [];
+
+        this.currentSeqmap = null;
+        this.ident = 0;
+
         txt = txt.replace(/\r/g, "");
         this.file = txt.split("\n");
         this.strtok_tmp = "";
@@ -89,16 +104,42 @@ class Eclmap {
         this.parse();
     }
     getMnemonic(num) {
-        if (this.opcodes[num]) return this.opcodes[num];
+        if (!this.is_new) {
+            if (this.opcodes[num])
+                return this.opcodes[num];
+        } else {
+            let ent = this.seqmapGet(this.ins, num);
+            if (ent != null)
+                return ent.name;
+        }
+        return `ins_${num}`;
+    }
+    getTimelineMnemonic(num) {
+        if (!this.is_new) {
+            if (this.timeline_opcodes[num])
+                return this.timeline_opcodes[num];
+        } else {
+            let ent = this.seqmapGet(this.timeline_ins, num);
+            if (ent != null)
+                return ent.name;
+        }
+
         return `ins_${num}`;
     }
     getGlobal(num) {
-        if (this.globals[num]) return this.globals[num];
+        if (!this.is_new) {
+            if (this.globals[num])
+                return this.globals[num];
+        } else {
+            let ent = this.seqmapGet(this.var, num);
+            if (ent != null)
+                return ent.name;
+        }
+
         return `[${num}]`;
     }
     err(txt) {
         alert(`eclmap parse error at line ${this.line}: ${txt}`);
-        this.file = null;
     }
     fgets() {
         ++this.line;
@@ -121,7 +162,9 @@ class Eclmap {
     parse() {
         const magic = this.fgets();
         if (magic != "eclmap") {
-            this.err("invalid magic - "+magic);
+            // parse new format, old format parsing will be removed in thhe future
+            this.is_new = true;
+            this.parseNew(magic);
             return;
         }
         let line;
@@ -146,5 +189,158 @@ class Eclmap {
 
             group[num] = token;
         }
+    }
+
+    control(arg) {
+        switch(arg) {
+            case "!ins_names":
+                this.ident = 1;
+                this.currentSeqmap = this.ins;
+                break;
+            case "!ins_signatures":
+                this.ident = 0;
+                this.currentSeqmap = this.ins_sig;
+                break;
+            case "!gvar_names":
+                this.ident = 1;
+                this.currentSeqmap = this.var;
+                break;
+            case "!gvar_types":
+                this.ident = 0;
+                this.currentSeqmap = this.var_types;
+                break;
+            case "!timeline_ins_names":
+                this.ident = 1;
+                this.currentSeqmap = this.timeline_ins;
+                break;
+            case "!timeline_ins_signatures":
+                this.ident = 0;
+                this.currentSeqmap = this.timeline_ins_sig;
+                break;
+            default:
+                this.err(`unknown control line: ${arg}`);
+                return 1;
+        }
+        return 0;
+    }
+
+    is_keyword(str) {
+        return [
+            "anim", "ecli", "sub", "timeline",
+            "var", "int", "float", "void",
+            "inline", "return", "goto", "unless",
+            "if", "else", "do", "while",
+            "times", "switch", "case", "default",
+            "break", "async", "global", "sin",
+            "cos", "sqrt", "rad", "false", "true"
+        ].indexOf(str) > -1;
+    }
+
+    validate_ident(str) {
+        let is_ident = str.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/) != null;
+        if (!is_ident) {
+            this.err(`'${str}' isn't a valid identifier`);
+            return 1;
+        }
+        if (str.substring(0, 4) == "ins_") {
+            this.err("mnemonic can't start with ins_");
+            return 1;
+        }
+        if (this.is_keyword(str)) {
+            this.err(`'${str}' is a keyword, ignoring`);
+            return 1;
+        }
+        return 0;
+    }
+
+    validate_type(str) {
+        if (str != "$" && str != "%") {
+            this.err(`unknown type '${str}'`);
+            return 1;
+        }
+        return 0;
+    }
+
+    validate_signature(str) {
+        // not implemented
+        return 0;
+    }
+
+    set(num, name) {
+        if (this.ident) {
+            if (this.validate_ident(name))
+                return 1;
+        } else if (this.currentSeqmap == this.globals_types) {
+            if (this.validate_type(name))
+                return 1;
+        } else {
+            if (this.validate_signature(name))
+                return 1;
+        }
+        this.seqmapSet(this.currentSeqmap, num, name);
+    }
+
+    parseNew(magic) {
+        this.control("!ins_names"); // default
+        this.seqmapLoad("!eclmap", this.set.bind(this), this.control.bind(this), magic);
+    }
+
+    seqmapLoad(magic, set, control, line) {
+        if (line != magic) {
+            this.err("invalid magic");
+            return;
+        }
+        while((line = this.fgets()) != null) {
+            // Remove comments.
+            line = line.split("#")[0];
+
+            let token = this.strtok(line, " \t\n");
+            if (!token) continue; // 0 tokens = empty line
+
+            if (token[0] == "!") {
+                control(token);
+                continue;
+            }
+
+            const num = parseInt(token);
+            if (isNaN(num)) {
+                this.err("key token is not a number");
+                return;
+            }
+
+            token = this.strtok(null, " \t\n");
+            if (!token) {
+                this.err("not enough tokens");
+                return;
+            }
+
+            if (token == "_")
+                token = ""; // specify empty strings with _
+
+            set(num, token);
+        }
+    }
+
+    seqmapSet(seqmap, num, name) {
+        seqmap.push({
+            num: num,
+            name: name
+        });
+    }
+
+    seqmapGet(seqmap, num) {
+        for (let i=seqmap.length-1; i>=0; --i) {
+            if (seqmap[i].num == num)
+                return seqmap[i];
+        }
+        return null;
+    }
+
+    seqmapFind(seqmap, name) {
+        for (let i=seqmap.length-1; i>=0; --i) {
+            if (seqmap[i].name == name)
+                return seqmap[i];
+        }
+        return null;
     }
 }
